9c4a9a17e5c58aa0faad9d69e92cbe8fb780d8e7
Alexandre1313 <seu@email.com>
2024-12-09 20:57:58 -0300

    091220242

--- src/components/componentesRomaneios/createServerSelectComponentDates.tsx
+++ src/components/componentesRomaneios/createServerSelectComponentDates.tsx
@@ -13,6 +13,7 @@ export async function CreateServerSelectComponentDates({ onSelectChange, project
     <div className={`flex flex-col justify-center items-start`}>     
       <select
         id="select-dates"
+        title="Selecione a data preferida"
         className={`flex w-[310px] bg-[#181818] py-2 px-3 text-[14px] text-zinc-400 no-arrow
                         outline-none cursor-pointer h-[35px] border border-zinc-800`}
         onChange={(event) => {
--- src/components/componentesRomaneios/createServerSelectComponentProjects.tsx
+++ src/components/componentesRomaneios/createServerSelectComponentProjects.tsx
@@ -11,6 +11,7 @@ export async function CreateServerSelectComponentProjects({ onSelectChange }: Cr
     <div className={`flex flex-col justify-center items-start`}>     
       <select
         id="select-projeto"
+        title="Selecione o projeto"
         className={`flex w-[310px] bg-[#181818] py-2 px-3 text-[14px] text-zinc-400 no-arrow
                         outline-none cursor-pointer h-[35px] border border-zinc-800`}
         onChange={(event) => {
