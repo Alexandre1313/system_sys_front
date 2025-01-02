0fb54049937b3360d4ab95f32c05a6ab70f636cd
Alexandre1313 <seu@email.com>
2025-01-02 00:36:42 -0300

    010120259

--- src/components/ComponentesGrade/ItemsGradeInputTextHor.tsx
+++ src/components/ComponentesGrade/ItemsGradeInputTextHor.tsx
@@ -9,13 +9,14 @@ export interface ItemsGradeInputTextHorProps {
 
 export default function ItemsGradeInputTextHor(props: ItemsGradeInputTextHorProps) {
     return (
-        <div className="flex items-center justify-center gap-x-3 bg-[#202020] rounded-md px-3">
+        <div className="flex items-center justify-center gap-x-3 
+        bg-gradient-to-r from-[#1d1818] to-transparent px-3">
             <label htmlFor={`${concat(props.labelName)}-input`} className="flex text-left text-[16px]
             text-zinc-500 tracking-[2px]">
                 {props.labelName}
             </label>
-            <input className="flex p-1 text-left text-[23px] bg-trans rounded-md  
-            outline-none text-emerald-500 h-auto max-w-[150px]"
+            <input className="flex p-1 text-left text-[26px] bg-trans rounded-md  
+            outline-none text-indigo-500 h-auto max-w-[150px]"
                 type="text"
                 name={`${concat(props.labelName)}`}
                 id={`${concat(props.labelName)}-input`}
