3c383e8504d65f0d7f69d2392e60ab78158542ff
Alexandre1313 <seu@email.com>
2025-03-30 17:22:38 -0300

    300320251

--- src/app/graf/page.tsx
+++ src/app/graf/page.tsx
@@ -5,7 +5,7 @@ import TitleComponentFixed from "@/components/ComponentesInterface/TitleComponen
 
 export default function Graf() {
     return (
-        <div className={`flex w-full p-20 min-h-screen max-h-screen flex-col justify-center items-center gap-y-5 bg-black`}>
+        <div className={`flex w-full px-6 pt-20 pb-8 min-h-screen max-h-screen flex-col justify-center items-center gap-y-5 bg-black`}>
             <TitleComponentFixed stringOne={`GRÁFICO`}/>
             <h1 className="text-center text-[30px] font-normal">Progresso dos Projetos</h1>
             <div className="flex w-full min-h-full">
--- src/components/ComponentesInterface/Grafico.tsx
+++ src/components/ComponentesInterface/Grafico.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 import { Bar } from 'react-chartjs-2';
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
 import { getGrafico } from '@/hooks_api/api';
+import { Grafo } from '../../../core';
 
 // Registrando as escalas e elementos do Chart.js
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
@@ -21,9 +22,9 @@ export default function Grafico() {
                 const projetos = await getGrafico();
 
                 // Preparar os dados da resposta da API
-                const labelsData = projetos.map((projeto: any) => projeto.nomeProjeto);
-                const quantidadeTotalData = projetos.map((projeto: any) => projeto.quantidadeTotal);
-                const quantidadeExpedidaData = projetos.map((projeto: any) => projeto.quantidadeExpedida);
+                const labelsData = projetos.map((projeto: Grafo) => projeto.nomeProjeto);
+                const quantidadeTotalData = projetos.map((projeto: Grafo) => projeto.quantidadeTotal);
+                const quantidadeExpedidaData = projetos.map((projeto: Grafo) => projeto.quantidadeExpedida);
 
                 // Atualizando o estado com os dados recebidos
                 setLabels(labelsData);
@@ -58,16 +59,16 @@ export default function Grafico() {
                 data: quantidadeTotal,
                 backgroundColor: 'rgba(75,192,192,0.6)',
                 borderColor: 'rgba(75,192,192,1)',
-                borderWidth: 1,
-                barThickness: 15,
+                borderWidth: 2,
+                barThickness: 40,
             },
             {
                 label: 'Quantidade Expedida',
                 data: quantidadeExpedida,
                 backgroundColor: 'rgba(153,102,255,0.6)',
                 borderColor: 'rgba(153,102,255,1)',
-                borderWidth: 1,
-                barThickness: 15,
+                borderWidth: 2,
+                barThickness: 40,
             },
         ],
     };
@@ -75,21 +76,45 @@ export default function Grafico() {
     // Renderiza o gráfico com os dados preparados
     return (
         <div className='flex w-full items-center justify-center'>
-            <Bar
+            <Bar className='flex w-full items-center justify-center'
                 data={data}
                 options={{
                     responsive: true,
                     plugins: {
                         tooltip: {
                             enabled: true,
+                            backgroundColor: 'rgba(0, 0, 0, 0.7)',  // Cor do fundo do tooltip
+                            titleColor: 'white',  // Cor do título do tooltip
+                            bodyColor: 'white',  // Cor do corpo do tooltip
+                            titleFont: {
+                                size: 16,  // Tamanho da fonte do título
+                                weight: 'bold',  // Peso da fonte do título
+                            },
+                            bodyFont: {
+                                size: 14,  // Tamanho da fonte do corpo
+                                weight: 'normal',  // Peso da fonte do corpo
+                            },
+                            padding: 10,  // Padding do tooltip                           
                         },
                     },
                     scales: {
                         x: {
                             stacked: false,
+                            ticks: {
+                                color: '#f8f8ff',
+                                font: {
+                                    size: 14,
+                                },
+                            },
                         },
                         y: {
                             beginAtZero: true,
+                            ticks: {
+                                color: '#f8f8ff',
+                                font: {
+                                    size: 14,
+                                },
+                            },
                         },
                     },
                 }}
