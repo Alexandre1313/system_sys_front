f36ac8bbefff1633a03fa1814f79db20f9da9328
Alexandre Cordeiro <alexandre.13a@gmail.com>
2025-08-15 15:11:03 -0300

    150820252

--- core/utils/tools.ts
+++ core/utils/tools.ts
@@ -10,7 +10,7 @@ const ip = "192.168.1.169";
 /**
  * Porta utilizada pelo servidor.
  */
-const port = process.env.NODE_ENV === 'production' ? '4997' : '4999';
+const port = process.env.NODE_ENV === 'production' ? '4997' : '4997';
 
 /**
  * Ordem customizada de tamanhos para uso em ordenação.
