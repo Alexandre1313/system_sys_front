03058373fb4f4557a62d58cdc633e275b611d696
Alexandre Cordeiro <alexandre.13a@gmail.com>
2025-08-24 22:50:09 -0300

    240820251

--- src/components/componentesEntradas/ItemsProjects.tsx
+++ src/components/componentesEntradas/ItemsProjects.tsx
@@ -27,7 +27,7 @@ export default function ItemsProjects({ onClick, item, index, embalagemId, itemT
     <div
       onClick={() => onClick(item, embalagemId, itemTamanhoId)} // Passando o item como argumento para a função `onClick`
       className={`group flex justify-start items-center w-full border-[0.000em] hover:bg-green-500 hover:bg-opacity-20
-            border-zinc-900 p-[0.60rem] rounded-md cursor-pointer ${bgColor}`}
+            border-zinc-900 p-[0.60rem] rounded-md cursor-pointer ${bgColor} h-12`}
     >
       <div className={`flex justify-start items-start gap-x-4 w-[10%] text-zinc-500`}>
         <span className={`text-[14px]`}>IT ID:</span>
@@ -43,11 +43,11 @@ export default function ItemsProjects({ onClick, item, index, embalagemId, itemT
       </div>
       <div className={`flex justify-start items-center gap-x-4 w-[20%] text-zinc-500`}>
         <span className={`text-[14px] text-zinc-500`}>TAMANHO:</span>
-        <span className={`text-[17px] ${classColor} group-hover:text-cyan-500`}>{itemTamanho}</span>
+        <span className={`text-[17px] ${classColor} group-hover:text-cyan-500 group-hover:!text-[27px]`}>{itemTamanho}</span>
       </div>
       <div className={`flex justify-start items-center gap-x-4 w-[20%] text-zinc-500`}>
         <span className={`text-[14px] text-zinc-500`}>CÓDIGO DE BARRAS:</span>
-        <span className={`text-[17px] font-thin ${classColor} group-hover:text-yellow-500`}>{itemBarcode}</span>
+        <span className={`text-[17px] font-thin ${classColor} group-hover:text-yellow-500 group-hover:!text-[27px]`}>{itemBarcode}</span>
       </div>
     </div>
   );
