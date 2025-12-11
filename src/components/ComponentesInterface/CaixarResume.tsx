31d320829289e9d476acdb64a42a8036b7d72d85
Alexandre Cordeiro <alexandre.13a@gmail.com>
2025-12-11 07:39:34 -0300

    2025-12-11-2

--- src/components/ComponentesInterface/CaixarResume.tsx
+++ src/components/ComponentesInterface/CaixarResume.tsx
@@ -10,8 +10,8 @@ export default function CaixaResume({ caixa }: CaixaResumeProps) {
         <div className={`shadow-lg flex flex-col min-w-[100%] lg:gap-y-3 gap-y-1 border border-zinc-800 rounded-lg p-1 min-h-[270px]`}>
             <div className={`flex flex-row w-full justify-start items-center gap-x-2 border border-zinc-800
                  rounded-lg lg:p-3 p-1`}>               
-                <span className={`lg:text-4xl text-2xl text-black font-bold`}>{caixa?.projeto}</span>
-                <span className={`lg:text-2xl text-xl text-black font-bold`}>{`- CX `}</span>
+                <span className={`lg:text-4xl text-2xl text-black font-bold`}>{'CAIXA Nº'}</span>
+                <span className={`lg:text-2xl text-xl text-black font-bold`}>{`-`}</span>
                 <span className={`lg:text-4xl text-2xl text-black font-bold`}>{caixa?.caixaNumber?.padStart(2, '0')}</span>                
             </div>
             <div className={`flex flex-row w-full justify-start items-center gap-x-2 px-3 border-b border-slate-200`}>
